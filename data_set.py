9d5ca3f9e00b695dbe2ef92d89864b1bc026e126
mperełkiewicz <mperelkiewicz@opi.org.pl>
2019-11-21 15:24:05 +0100

    Simple BCELoss version

--- data_set.py
+++ data_set.py
@@ -10,25 +10,44 @@ import numpy as np
 
 class SentimentDataSet(dataset.Dataset):
 
-    def __init__(self, domain: str):
+    def __init__(self, domain: str, n=None):
         self.domain = domain
+        self.n = n
         self.data: pd.DataFrame = MultiDomainSentimentDataReader.read(domain)
+        self._dict = {}
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
-        return self.data.iloc[index]
+        item = self.data.iloc[index]
+        return self.one_hot_sentence([item[0] for item in item.acl_processed]), 1 if 'positive' == item.sentiment else 0
+
+    def one_hot_word(self, word: str):
+        if word in self._dict:
+            vector = np.zeros(self.n)
+            vector[self._dict[word][0]] = 1
+            return vector
+        else:
+            return np.zeros(self.n)
+
+    def one_hot_sentence(self, words: List[str]):
+        vector = np.zeros(self.n)
+        for word in words:
+            vector = np.add(vector, self.one_hot_word(word))
+        return vector
 
 
 class MultiDomainSentimentDataSet:
 
     def __init__(self, src_domain_nm: str, tgt_domain_nm: str, n=None):
         print("# MultiDomainSentimentDataSet building [source domain: {}, target domain: {}]".format(src_domain_nm, tgt_domain_nm))
-        self.src_ds = SentimentDataSet(src_domain_nm)
-        self.tgt_ds = SentimentDataSet(tgt_domain_nm)
+        self.src_ds = SentimentDataSet(src_domain_nm, n)
+        self.tgt_ds = SentimentDataSet(tgt_domain_nm, n)
         self.n = n
         self._dict = self._build_dictionary(n)
+        self.src_ds._dict = self._dict
+        self.tgt_ds._dict = self._dict
 
     def _build_dictionary(self, n=None):
         _dict = {}
@@ -47,20 +66,6 @@ class MultiDomainSentimentDataSet:
         for i, key in enumerate(_sorted_dict):
             _sorted_dict[key] = (i, _sorted_dict[key])
         return _sorted_dict
-
-    def one_hot_word(self, word: str):
-        if word in self._dict:
-            vector = np.zeros(self.n)
-            vector[self._dict[word][0]] = 1
-            return vector
-        else:
-            return np.zeros(self.n)
-
-    def one_hot_sentence(self, words: List[str]):
-        vector = np.zeros(self.n)
-        for word in words:
-            vector = np.add(vector, self.one_hot_word(word))
-        return vector
 
 
 
--- run.py
+++ run.py
@@ -1,8 +1,78 @@
 import config
+import torch
+import sys
+from torch.utils.data import DataLoader
+from model import Feedforward
 from data_set import SentimentDataSet, MultiDomainSentimentDataSet
 from pprint import pprint
 
-dataset = MultiDomainSentimentDataSet(config.SOURCE_DOMAIN, config.TARGET_DOMAIN, n=5000)
+if __name__ == '__main__':
+    # CUDA for PyTorch
+    import os
 
-for item in dataset.src_ds:
-    pprint(dataset.one_hot_sentence([word[0] for word in item.acl_processed]))
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda:0" if use_cuda else "cpu")
+    torch.cuda.set_device(device)
+
+    multidataset = MultiDomainSentimentDataSet(config.SOURCE_DOMAIN, config.TARGET_DOMAIN, n=5000)
+
+    params = {'batch_size': 16,
+              'shuffle': True,
+              'num_workers': 1}
+    max_epochs = 100
+
+    # Generators
+    training_generator = DataLoader(multidataset.src_ds, **params)
+    validation_generator = DataLoader(multidataset.tgt_ds, **params)
+
+    model = Feedforward(5000, 10)
+    model.to(device)
+
+    criterion = torch.nn.BCELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+    # Loop over epochs
+    for epoch in range(max_epochs):
+        # Training
+        batch = 0
+        loss_all = 0
+        for local_batch, local_labels in training_generator:
+            batch += 1
+            # Transfer to GPU
+            local_batch, local_labels = local_batch.to(device, torch.float), local_labels.to(device, torch.float)
+
+            # Model computations
+            optimizer.zero_grad()
+            # Forward pass
+            y_pred = model(local_batch)
+            # Compute Loss
+            loss = criterion(y_pred.squeeze(), local_labels)
+            loss_all += loss.item()
+            sys.stdout.write('\rEpoch {}, Batch {}, train loss: {}'.format(epoch, batch, round(loss_all / batch, 4)))
+            sys.stdout.flush()
+            # Backward pass
+            loss.backward()
+            optimizer.step()
+        sys.stdout.write('\n')
+
+        # Validation
+        with torch.set_grad_enabled(False):
+            batch = 0
+            loss_all = 0
+            for local_batch, local_labels in validation_generator:
+                # Transfer to GPU
+                batch += 1
+                local_batch, local_labels = local_batch.to(device, torch.float), local_labels.to(device, torch.float)
+
+                # Model computations
+                y_pred = model(local_batch)
+
+                loss = criterion(y_pred.squeeze(), local_labels)
+
+                loss_all += loss.item()
+                sys.stdout.write(
+                    '\rValidation: Epoch {}, Batch {}, train loss: {}'.format(epoch, batch, round(loss_all / batch, 4)))
+                sys.stdout.flush()
+            sys.stdout.write('\n')
